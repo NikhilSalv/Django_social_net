1adf559e875471a8204998f0dae200cdfd327851
Nikhil <x20179529@students.ncirl.ie>
2024-06-16 11:41:25 +0100

    Add : Created Profile model & superuser

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField(blank=False)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='default.png')
+    location = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.user.username
