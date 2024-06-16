ce6efc4a14aef0bce85b585027bff3af1bb49799
Nikhil <x20179529@students.ncirl.ie>
2024-06-16 15:30:01 +0100

    Add : Created post method for form in sign up page

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('index/',views.index, name = 'index')
+    path('index/',views.index, name = 'index'),
+    path('signup/',views.signup, name= 'signup')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        return HttpResponse("user name is : ", username)
+    else:
+        return render(request, 'signup.html')
