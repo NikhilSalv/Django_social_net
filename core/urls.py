a0adab468e8988a0fdb3b16ecc41bea7bab9b718
Nikhil <x20179529@students.ncirl.ie>
2024-06-17 13:00:52 +0100

    Add : SignUp,SignIn and logout features added

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('index/',views.index, name = 'index'),
-    path('signup/',views.signup, name= 'signup')
+    path('',views.index, name = 'index'),
+    path('signup/',views.signup, name= 'signup'),
+    path('signin/',views.signin, name= 'signin'),
+    path('logout/',views.logout, name= 'logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
